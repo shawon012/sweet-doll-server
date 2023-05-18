28ce8b55c2b79c25a0dbb05dcdc09666f3f1326b
shawon012 <shawon.shakib01@gmail.com>
2023-05-18 12:47:34 +0600

    json web token instaled and versel json file added and basic index js work done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+  app.get('/', (req, res) =>{
+    res.send('My DoooL Shop will open soon')
+  })
+
+  app.listen(port, () =>{
+    console.log('Shop will open soon')
+  })
