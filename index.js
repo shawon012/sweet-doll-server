698a6bd9a48bcf4be50f5e9282fc2b5f2367f8f4
shawon012 <shawon.shakib01@gmail.com>
2023-05-18 16:20:44 +0600

    delete one added

--- index.js
+++ index.js
@@ -74,7 +74,12 @@ async function run() {
         res.send(result)
     })
 
-    
+    app.delete('/products/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await productCollection.deleteOne(query);
+        res.send(result);
+    })
 
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
