6fffdaf53fb908d92242733edb897afe1f013782
shawon012 <shawon.shakib01@gmail.com>
2023-05-20 19:07:29 +0600

    jwt problem handling

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
       })
 
     //   single product
-    app.get('/products/:id', verifyJwt, async(req, res) =>{
+    app.get('/products/:id', async(req, res) =>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const options = {
